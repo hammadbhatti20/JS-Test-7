1dd7caa4c65088287801f026a8f0dd3a3c336132
hammadbhatti20 <hammadaslam1052000@gamil.com>
2024-03-25 15:07:36 +0500

    ASSIGNMENT # 7 half complete

---
+++ app.js
@@ -0,0 +1,64 @@
+//Chapter No: 26-30
+
+//Q1
+// var userInput = +prompt("Enter Num Value");
+// var num = userInput;
+// var roundOff = Math.round(userInput);
+// var floorVal = Math.floor(userInput);
+// var ceilVal = Math.ceil(userInput);
+
+// document.write(`number: ${userInput} </br> round off value: ${roundOff} </br> floor value: ${floorVal} </br> ceil value: ${ceilVal}`);
+
+//Q2
+// var userInput = +prompt("Enter Neg Num Value");
+// var num = userInput;
+// var roundOff = Math.round(userInput);
+// var floorVal = Math.floor(userInput);
+// var ceilVal = Math.ceil(userInput);
+
+// document.write(`number: ${userInput} </br> round off value: ${roundOff} </br> floor value: ${floorVal} </br> ceil value: ${ceilVal}`);
+
+//Q3
+// var userInput = +prompt("Enter Num Value");
+// var negVal = Math.ceil(Math.random() * userInput + 1);
+
+// document.write(`The absolute value of ${userInput} is ${negVal}`);
+
+//Q4
+// var dice = 7;
+// var result = Math.floor(Math.random() * dice);
+
+// document.write(`random dice value: ${result}`);
+
+//Q5
+// var heads = 2;
+// var result = Math.ceil(Math.random() * heads);
+
+// var tails = 2;
+// var result2 = Math.ceil(Math.random() * tails);
+
+// document.write(`${result} </br> random coin value: Heads </br> </br> ${result2} </br> random coin value: Tails`);
+
+//Q6
+// var randNUm = Math.ceil(Math.random() * 100);
+
+// document.write(`random number between 1 and 100: ${randNUm}`);
+
+//Q7
+// var userInput = prompt("Enter Your Weight");
+// var roundVal = Math.round(userInput);
+// var floorVal = Math.floor(userInput);
+// var ceilVal = Math.ceil(userInput);
+
+// document.write(`The weight of user is kilograms ${userInput} </br> The weight of user is kilograms ${roundVal} </br> The weight of user is kilograms ${floorVal} </br> The weight of user is kilograms ${ceilVal} `);
+
+//Q8
+// var secNum = Math.ceil(Math.random() * 10);
+// var userInput = +prompt("Enter Your Value From 1 to 10");
+// console.log(secNum, userInput);
+
+// if(userInput == secNum) {
+//     alert("Congrates you win");
+// } else {
+//     alert("Better luck next time");
+// }
