b7282fcad7890e15fa3284ae196e4b7bb40a100d
hammadbhatti20 <hammadaslam1052000@gamil.com>
2024-03-26 15:46:40 +0500

    ASSIGNMENT # 7 complete

--- app.js
+++ app.js
@@ -62,3 +62,164 @@
 // } else {
 //     alert("Better luck next time");
 // }
+
+
+//Chapter 26 (Rounding Numbers)
+
+//Q1
+// var num = 5.5;
+// console.log(num);
+// console.log(Math.round(num));
+
+//Q2
+// var origNum = 3.5;
+// var roundNum = Math.round(origNum);
+// console.log(origNum);
+// console.log(roundNum);
+
+//Q3
+// var origNum = 3.5;
+// var roundNum = Math.floor(origNum);
+// console.log(origNum);
+// console.log(roundNum);
+
+//Q4
+// var origNum = 3.5;
+// var roundNum = Math.round(origNum);
+// console.log(origNum);
+// console.log(roundNum);
+
+//Q5
+// var origNum = 0.5;
+// var roundNum = Math.floor(origNum);
+// console.log(origNum);
+// console.log(roundNum);
+
+
+//Chapter 27 (Random Numbers)
+
+//Q1
+// var randNum = Math.floor(Math.random() * 51);
+// console.log(randNum);
+
+//Q2
+// var ranNum = Math.random();
+// var newNum = ranNum;
+// console.log(newNum); 
+
+//Q3
+// var dice = Math.floor(Math.random() * 6) + 1;
+// console.log(dice);
+
+//Q4
+// var toss = Math.floor(Math.random() * 2);
+// if(toss == 0) {
+//     alert(`Heads Win`);
+//     console.log(toss);
+// } else {
+//     alert(`Tails Win`);
+//     console.log(toss);
+// }
+
+
+//Chapter 28, 29 (Converting Strings)
+
+//Q1
+// var num = 123;
+// var str = num.toString();
+// console.log(num);
+// console.log(str);
+
+//Q2
+// var str = '123';
+// var num = Number(str);
+// console.log(str);
+// console.log(num);
+
+//Q3
+// var str = "3.14159";
+// var floatValue = parseFloat(str);
+// console.log(str);
+// console.log("Floating-point number:", floatValue);
+
+//Q4
+// // Function to check if the string can be converted to an integer
+// function isInteger(str) {
+//     return /^\d+$/.test(str) || /^-\d+$/.test(str);
+// }
+
+// // Function to check if the string can be converted to a decimal
+// function isDecimal(str) {
+//     return /^-?\d*\.?\d+$/.test(str);
+// }
+
+// // Test cases
+// let integerStr = "123";
+// let decimalStr = "123.45";
+// let nonNumericStr = "abc";
+// let negativeIntegerStr = "-123";
+// let negativeDecimalStr = "-123.45";
+
+// console.log(integerStr + " can be converted to integer:", isInteger(integerStr));
+// console.log(decimalStr + " can be converted to decimal:", isDecimal(decimalStr));
+// console.log(nonNumericStr + " can be converted to integer:", isInteger(nonNumericStr));
+// console.log(negativeIntegerStr + " can be converted to integer:", isInteger(negativeIntegerStr));
+// console.log(negativeDecimalStr + " can be converted to decimal:", isDecimal(negativeDecimalStr));
+
+//Q5
+// var num = 123;
+// var str = num.toString();
+// console.log(num);
+// console.log(str);
+
+//Q6
+// var num = 42;
+// var str = num.toString();
+// console.log(num);
+// console.log(str);
+
+//Q7
+// var str = '3.14';
+// var num = Number(str);
+// console.log(str);
+// console.log(num);
+
+
+//Chapter 30 (Controlling the length of decimals)
+
+//Q1
+// var num = 3.5;
+// var roundNum = Math.round(num);
+// var str = roundNum.toString();
+// var newNum = str;
+// console.log(num);
+// console.log(roundNum);
+// console.log(str);
+// console.log(newNum);
+
+//Q2
+// var a = 1.5;
+// var str = a.toString();
+// var num = Number(str);
+// a = num;
+// console.log(a);
+// console.log(str);
+// console.log(num);
+// console.log(a);
+
+//Q3
+// var num = 2
+// if ((Math.round(num * 100) / 100).toFixed(2).toString().length > 4) {
+//     console.log(true);
+// } else {
+//     console.log(false);
+// }
+
+//Q4
+// var decNum = 1.50;
+// var num = Math.round(decNum);
+// var str = num.toString();
+// alert(`${num} is converted to String ${str}`);
+// console.log(decNum);
+// console.log(num);
+// console.log(str);
